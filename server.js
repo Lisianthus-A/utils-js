8b3111e0059c65368842a687bb2e4b7d3e30566e
Lisianthus-A <lisianthus.zeng@gmail.com>
2021-07-27 01:10:26 +0800

    update

--- server.js
+++ server.js
@@ -6,27 +6,34 @@ const { exec } = require('child_process');
 const port = 4100;  //运行端口
 const openBrowser = true;  //自动打开浏览器
 
-const sendFile = (filePath, res) => {
+const sendFile = (filePath, res) => new Promise(resolve => {
+    let result;
+    filePath = decodeURIComponent(filePath);
     try {
         const file = fs.readFileSync(filePath);
         res.write(file);
+        result = true;
     } catch (e) {
-        console.log(e);
         res.writeHead(404);
+        result = false;
     }
     res.end();
-}
+    resolve(result);
+});
 
 const server = http.createServer((req, res) => {
     const { url } = req;
+    let filePath;
     if (url === '/') {  // 根地址，返回 index.html
-        const filePath = path.join(__dirname, 'index.html');
-        sendFile(filePath, res);
+        filePath = path.join(__dirname, 'index.html');
     } else {  // 返回相应文件
-        const pathname = url.match(/[\/\w\-\.]+/)?.[0] || '';
-        const filePath = path.join(__dirname, pathname);
-        sendFile(filePath, res);
+        const pathname = url.match(/[\/\w\-\.%!_:\(\)]+/)?.[0] || '';
+        filePath = path.join(__dirname, pathname);
     }
+    sendFile(filePath, res).then(result => {
+        // result && console.log(`[ok]: ${url}`);
+        !result && console.log(`[err]: ${url}`);
+    })
 });
 
 server.listen(port, (err) => {
